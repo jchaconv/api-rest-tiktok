72a2114e1c6911349beeb81fe8006c8d2ae7dd68
Julio Cesar Aaron Chacon Vilela <jchaconv@canvia.com>
2021-06-13 22:37:06 -0500

    Se agrega el método PUT para modificar usuarios.

--- src/main/java/com/example/demo/controller/UserTikTokController.java
+++ src/main/java/com/example/demo/controller/UserTikTokController.java
@@ -1,20 +1,17 @@
 package com.example.demo.controller;
 
-import com.example.demo.exception.UserTikTokNotFoundException;
 import com.example.demo.model.UserTikTok;
 import com.example.demo.repositories.UserTikTokRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Date;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api/v1")
 public class UserTikTokController {
-    //ver mr.robot en cuevana
 
     private UserTikTokRepository repository;
 
@@ -22,14 +19,15 @@ public class UserTikTokController {
         this.repository = repository;
     }
 
-    //Es triste amar sin ser amado u.u
-    //Pero más triste es cerrar word sin
-    // haber guardado ToT
-
     @GetMapping(path="/users")
     public @ResponseBody Iterable<UserTikTok> getAllUsers() {
         // This returns a JSON or XML with the users
         return repository.findAll();
+    }
+
+    @GetMapping(path="/users/{id}")
+    public Optional<UserTikTok> getUserById(@PathVariable Long id){
+        return repository.findById(id);
     }
 
     @PostMapping(path="/add")
@@ -37,6 +35,26 @@ public class UserTikTokController {
         UserTikTok created = repository.save(userTikTok);
 
         return new ResponseEntity<UserTikTok>(HttpStatus.CREATED);
+    }
+
+    @PutMapping(path= "/users/{id}")
+    public UserTikTok replaceUserTikTok(@RequestBody UserTikTok newUserTikTok,
+                                        @PathVariable Long id){
+
+        return repository.findById(id)
+                .map(userTikTok -> {
+                    userTikTok.setName(newUserTikTok.getName());
+                    userTikTok.setNickname(newUserTikTok.getNickname());
+                    userTikTok.setDescription(newUserTikTok.getDescription());
+                    userTikTok.setNumberOfFollowers(newUserTikTok.getNumberOfFollowers());
+                    userTikTok.setNumberOfViewers(newUserTikTok.getNumberOfViewers());
+                    userTikTok.setNumberOfLikes(newUserTikTok.getNumberOfLikes());
+                    return repository.save(userTikTok);
+                })
+                .orElseGet(() -> {
+                    newUserTikTok.setId(id);
+                    return repository.save(newUserTikTok);
+                });
     }
 
 
